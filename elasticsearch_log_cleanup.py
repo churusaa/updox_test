1ae451f039614a42161716853318e22a4c82a562
Nathanael <nathanael.j.duke@gmail.com>
2017-10-08 17:50:51 -0400

    Add initial files
    Getting this started

---
+++ elasticsearch_log_cleanup.py
@@ -0,0 +1,23 @@
+
+
+The second challenge, a Python script that will run as a daily job. 
+It should delete timestamped ElasticSearch indices (indices with a name pattern like "name-YYYY.MM.DD") 
+that are older than a configurable number of days.
+
+## ElasticSearch index cleanup
+##
+
+import time, os, sys
+
+# Number of days of indices to keep
+# retain_days = '10'
+retain_days = 10
+
+# ElasticSearch index directory
+# index_directory = '/var/lib/elasticsearch/data/'
+index_directory = '/var/lib/elasticsearch/data/'
+
+now = time.time()
+
+for files in os.listdir(index_directory):
+	if
