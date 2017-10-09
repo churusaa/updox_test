9b0e63749915000e42dac2cafefc8e2f3b22e282
Nathanael Duke <churusaa@gmail.com>
2017-10-09 00:10:01 -0400

    Fixed the testing direction of the log purge

--- elasticsearch_log_cleanup.py
+++ elasticsearch_log_cleanup.py
@@ -1,7 +1,3 @@
-#The second challenge, a Python script that will run as a daily job. 
-#It should delete timestamped ElasticSearch indices (indices with a name pattern like "name-YYYY.MM.DD") 
-#that are older than a configurable number of days.
-
 ## ElasticSearch index cleanup
 ##
 
@@ -9,7 +5,7 @@ import os, datetime
 
 # Number of days of indices to keep
 # retain_days = 10
-retain_days = 10
+retain_days = 0
 
 # Everything that comes before the date string in the file name
 # (Don't include the path. That's defined in the next variable)
@@ -28,15 +24,6 @@ index_directory = '/var/lib/elasticsearch/data/'
 # date_format = "%Y.%m.%d"
 date_format = "%Y.%m.%d"
 
-make_days = 100 
-if not os.path.exists(index_directory):
-  os.makedirs(index_directory)
-while make_days >= 0:
-  currdate = datetime.datetime.now() - datetime.timedelta(days=make_days)
-  F = open(index_directory + matching_pattern + currdate.strftime(date_format) + file_suffix, "w")
-  make_days = make_days - 1
-
-#for files in os.listdir(index_directory):
 # Logs older than X days old will also be left alone. Defaults to 3 years
 # remove_days = 1095
 remove_days = 1095
@@ -44,8 +31,7 @@ remove_days = 1095
 while remove_days > retain_days:
   currdate = datetime.datetime.now() - datetime.timedelta(days=remove_days)
   file_to_remove = index_directory + matching_pattern + currdate.strftime(date_format) + file_suffix
-  print "Attempting to remove file ", file_to_remove 
   if os.path.isfile(file_to_remove):
     os.remove(file_to_remove) 
-    print "Successfully removed", file_to_remove
+    print "Removed", file_to_remove
   remove_days = remove_days - 1	
